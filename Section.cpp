8c9e6cd007cc200daf0bdc2192946c99157d837e
Bruthor <Sterling2@mail.usf.edu>
2016-03-28 14:41:50 -0400

    Update Section.cpp

--- Section.cpp
+++ Section.cpp
@@ -1,6 +1,8 @@
 //Test
 
 #include <iostream>
+#include <string>
+#include <cassert>
 #include "Section.h"
 
 using namespace std;
@@ -8,7 +10,19 @@ using namespace std;
 Section::Section(section_name)
 {}
 
+void Add_Seat(const Seat* new_seat)
+{
+    assert(number_of_seats < MAX_SEATS_PER_ROW);
+    seats[number_of_seats++] = new_seat;
+}
+
 void Section::Display() const
 {
-    cout << "Section " << section_name << endl; //NEEDS EDITTING. THIS IS A SUPER DUPER ROUGH QUICK TYPE UP
+    cout << "Section " << section_name << " has "
+        << number_of_seats << " seats" << endl;
+    for (int i = 0; i < number_of_seats; ++i)
+    {
+        cout << "\t";		// Indent the Seat description
+        seats[i]->Display();
+    }
 }
