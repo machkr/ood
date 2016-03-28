44d4baaaf1ffca365826d25c58ce92238e27a12f
Bruthor <Sterling2@mail.usf.edu>
2016-03-27 21:02:44 -0400

    Super quick update before I watch Daredevil
    Just wanted to at least add SOMETHING tonight. This isn't even close to being done, but it's better than nothing.

--- Section.h
+++ Section.h
@@ -1,1 +1,18 @@
 //Just making it for now, also part of me figuring out how Github works. 
+
+#pragma once
+#include <string>
+#include <seat.h> //To be determined if we need this header file included. 
+
+using namespace std;
+
+class Section
+{
+private:
+    string section_name; //TBD if we even want to use this, the diagram had no such variable
+
+public:
+    Section(seat Seat);
+
+    void Display() const;
+};
