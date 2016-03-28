5be6436e5133d235f09d98db9eb1bb3bf401441e
Bruthor <Sterling2@mail.usf.edu>
2016-03-27 21:00:11 -0400

    Just a quick short update, may not compile, be warned

--- Section.cpp
+++ Section.cpp
@@ -1,1 +1,14 @@
 //Test
+
+#include <iostream>
+#include "Section.h"
+
+using namespace std;
+
+Section::Section(seat S)
+{}
+
+void Section::Display() const
+{
+    cout << "Section " << endl; //NEEDS EDITTING. THIS IS A SUPER DUPER ROUGH QUICK TYPE UP
+}
