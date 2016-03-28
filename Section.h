79904bff42e6f03d0181a94fbd9cdb0d36008e95
Bruthor <Sterling2@mail.usf.edu>
2016-03-28 12:36:18 -0400

    Added Seat_row functionalities because they're the same

--- Section.h
+++ Section.h
@@ -2,17 +2,26 @@
 
 #pragma once
 #include <string>
-#include <seat.h> //To be determined if we need this header file included. 
+#include "Seat.h" //To be determined if we need this header file included. 
 
 using namespace std;
+
+static const int MAX_SEATS_PER_SECTION = 1000;
 
 class Section
 {
 private:
     string section_name; //TBD if we even want to use this, the diagram had no such variable
+    const Seat* seats[MAX_SEATS_PER_SECTION];
+    int  number_of_seats;
+  
 
 public:
-    Section(section_name);
-
+    Section(const string& section_name);
+    void Add_Seat(const Seat* new_seat);
     void Display() const;
+    
+    int Number_of_Seats() const { return number_of_seats; }
+    
+    const Seat* Get_Seat(int idx) const { return seats[idx]; };
 };
