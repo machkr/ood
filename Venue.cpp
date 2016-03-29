df45d73ae3daaabcbc6624d306cb79c6b8f243c2
Brad Daniels <brad@sleinad.net>
2016-03-29 00:11:12 -0400

    Defined Display_All() to Venue

--- Venue.cpp
+++ Venue.cpp
@@ -1,6 +1,8 @@
 #include <string.h>
 #include <iostream>
 #include <cassert>
+#include <iterator>
+#include <map>
 #include "Venue.h"
 #include "Seat_Row.h"
 using namespace std;
@@ -37,7 +39,53 @@ void Venue::Display() const
 
 void Venue::Display_All() const
 {
-    // todo
+    cout << "The new venue:\n" << venue_name << "\n" << address.street << "\n" 
+        << address.city << ", " << address.state << " " << address.zip << "\n";
+    for (int i = 0; i < number_of_seat_rows; i++) 
+    {
+        cout << "Row" << seat_rows[i]->Get_Row_Name() << " Seats 1 - " <<
+            seat_rows[i]->Number_of_Seats() << "\n";
+    }
+    cout << "\n";
+
+    for (int i = 0; i < number_of_sections; i++) 
+    {
+        cout << "Section " << sections[i]->Get_Section_Name() << "\n";
+
+        // Sort seats by Row, put into a map
+        std::map<string, Seat_Row*> rows;
+        for (int j = 0, Section* s = sections[i]; j < s->Number_of_Seats(); j++)
+        {
+            string row_name = s->Get_Seat(j)->Get_Seat_Row()->Get_Row_Name();
+            
+            // If row is in map, add seat to row
+            // Else, add a new row to map
+            if (rows.find(row_name) != rows.end) 
+            {
+                rows[row_name]->Add_Seat(s->Get_Seat(j));
+            } 
+            else
+            {
+                rows[row_name] = Seat_Row(row_name);
+                rows[row_name]->Add_Seat(s->Get_Seat(j));
+            }
+        }
+        
+        // Iterate through map
+        map<string, Seat_Row*>::iterator it;
+        for (it = rows.begin(); it != rows.end(); it++) 
+        {
+            cout << "Row " << it->first << "  Seats 1 - " << 
+                it->second->Number_of_Seats() << "\n";
+        }
+        
+        // Destroy map
+        for (it = rows.begin(); it != rows.end(); it++) 
+            delete it->second;
+
+    }
+    cout << "\n";
+
 }
 
 // Return number of seats
