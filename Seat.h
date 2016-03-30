349ae6a968319a9208a4f84463f19ccfa945a466
alando1 <arodriguez7@mail.usf.edu>
2016-03-29 23:58:06 -0400

    Update Seat.h

--- Seat.h
+++ Seat.h
@@ -1,16 +1,22 @@
 #pragma once
 #include <string>
+#include "Seat_Row.h"
+#include "Section.h"
 using namespace std;
 
 class Seat
 {
 private:
-    string seat_row_name;
-    int seat_number;
+	string seat_row_name;
+	int seat_number;
+	Seat_Row* row;
+	Section* section;
+
 
 public:
-    Seat(string Row_Name,
-        int Seat_Number);
-
-    void Display() const;
+	Seat(string Row_Name, int Seat_Number, Seat_Row* Row);
+	void Set_Section(Section* new_section);
+	Section* Get_Section();
+	
+	void Display() const;
 };
