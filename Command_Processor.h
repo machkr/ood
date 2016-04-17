68e4b30b61b43c22030a8dcdafbce9d04187e211
Sterlin <Sterlin@STERLING-PC>
2016-04-17 18:06:25 -0400

    Updated Command.h

--- Command_Processor.h
+++ Command_Processor.h
@@ -1,10 +1,29 @@
 #pragma once
+#include <string>
+#include "Menu.h" //We'll need this when we create a menu.
+#include "Venue.h"
 
+enum Command_States { Initial, State_Selected, Done};
 using namespace std;
 
 class Command_Processor
 {
 public:
-	Command_Processor();
-	~Command_Processor();
+	static void Process_Commands(Venue** venues_, int* nr_venues_);
+	
 };
+private:
+	static const int NR_CMD_STATES = (int)Done + 1;
+	static Command_States command_state;
+	static Menu* menus[NR_CMD_STATES];
+	Command_Processor(void);
+	static void Create_Menus();
+	static void Process_Command_1(const string& cmd);
+	static void Process_Command_2(const string& cmd);
+	static void Select_Venue();
+	static Venue** Venues;
+	static int* nr_venues;
+	static Venue* selected_venue;
+	static void Output_XML()
+
+	~Command_Processor();
