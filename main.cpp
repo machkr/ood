80d6ccd326bf342cf84ba58718a99a104acd499c
Sterlin <Sterlin@STERLING-PC>
2016-04-17 20:50:56 -0400

    Made a few slight changes to main in preparation for the future function / file integrations.

--- main.cpp
+++ main.cpp
@@ -3,13 +3,18 @@
 #include "Venue.h"
 #include "Venue_From_User.h"
 #include "Venue_From_XML.h"
+#include "menu.h"
+#include "Command_Processor.h"
 
 using namespace std;
 
 int main()
 {
-	cout << "This is the Level 0 Ticket Booth program!" << endl;
+	Venue* venues[999];
+	int nr_states = 0;
 
+	cout << "This is the Level 0 Ticket Booth program!" << endl;
+	nr_venues
 	Venue* venue = Venue_From_XML::Get_Venue_From_XML();
 	venue->Display_All();
 
